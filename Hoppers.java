71c7f2844edb120fb27152e671bc855a8ddfc732
Tucker Tavarone <ttavarone@tuckers-air.siena.edu>
2018-03-10 19:58:13 -0500

    Updated and refined code

--- Hoppers.java
+++ Hoppers.java
@@ -1,308 +1,180 @@
 import java.util.ArrayList;
+
 /**
- * A text based version of the game Hoppers
- *
+ * A text-based version of the game Hoppers
+ * 
  * @author (Tucker Tavarone)
- * @version (2/28/18)
+ * @version (3/7/18)
  */
-public class Hoppers
+public class Hoppers 
 {
-    private class lilyPad
-    {
-        private int padNum;
-        private boolean hasFrog;
-        private boolean isRedFrog;
-        private boolean jumped;
-        private boolean isMid;
-        private Connection head;
-
-        private lilyPad(int padNumIn, boolean hasFrogIn, boolean isRedFrogIn)
-        {
-            padNum = padNumIn;
-            hasFrog = hasFrogIn;
-            isRedFrog = isRedFrogIn;
-            isMid = false;
-        }
-
-        private boolean isMidPoint()
-        {
-            return isMid;
-        }
-
-        private boolean hasFrog()
-        {
-            return hasFrog;
-        }
-
-        private boolean isRedFrog()
-        {
-            return isRedFrog;
-        }
-
-        private int getPadNum()
-        {
-            return padNum;
-        }
-
-        private void placeFrog()
-        {
-            hasFrog = true;
-        }
-
-        private void setToRedFrog()
-        {
-            isRedFrog = true;
-        }
-        
-        private void removeRedFrog()
-        {
-            isRedFrog = false;
-        }
-
-        private void setToMid()
-        {
-            isMid = true;
-        }
-        
-        private void removeFrog()
-        {
-            hasFrog = false;
-        }
-    }
-
-    private class Connection
-    {
-        private int dest;
-        private Connection next;
-
-        public Connection(int dst, Connection n)
-        {
-            dest = dst;
-            next = n;
-        }
-    }
-
-    private lilyPad[] board;
-    private int remainingFrogs;
-    private int firstFrogPos;
-    boolean[][][] testTheMove;
+    private boolean[][][] testTheMove = new boolean[13][13][13]; //positions array
+    private boolean[] board = new boolean[13]; //board containing the current frog positions
+    private int redFrogPos; // where the red frog is
+    private ArrayList<Integer> jumps = new ArrayList<Integer>();//a list of the jumps made
+    private int remainingFrogs;//remaining frogs on the board
 
     /**
-     * For adding connections from lilyPad object to lilyPad object
-     * Helps to create the adjaceny list of connections
-     * 
-     * @param
-     * @param
+     * Constructor for objects of type Hoppers.
      */
-    public void addConnect(int source, int dest)
-    {
-        board[source].head = new Connection(dest, board[source].head);
-    }
-
     public Hoppers()
     {
-        board = new lilyPad[13]; // initializes the board to a specific board size
-        remainingFrogs = 0;
-
-        for(int i = 0; i < board.length; i++){
-            board[i] = new lilyPad(i, true, false);
+        
+        for(int i = 0; i<13; i++){//initialize the board to all false
+            board[i] = false;
         }
 
-        // this space contains initilization for the entire board and its connections
-        //as well as the other instance variables
+        testTheMove = new boolean[13][13][13]; //initialize the array containing all the
+        //possible moves
 
-        addConnect(0, 1);
-        addConnect(0, 3);
-        addConnect(0, 5);
-
-        addConnect(1, 0);
-        addConnect(1, 3);
-        addConnect(1, 6);
-        addConnect(1, 4);
-        addConnect(1, 2);
-
-        addConnect(2, 1);
-        addConnect(2, 4);
-        addConnect(2, 7);
-
-        addConnect(3, 0);
-        addConnect(3, 1);
-        addConnect(3, 5);
-        addConnect(3, 6);
-
-        addConnect(4, 1);
-        addConnect(4, 2);
-        addConnect(4, 6);
-        addConnect(4, 7);
-
-        addConnect(5, 0);
-        addConnect(5, 3);
-        addConnect(5, 6);
-        addConnect(5, 8);
-        addConnect(5, 10);
-
-        addConnect(6, 1);
-        addConnect(6, 3);
-        addConnect(6, 4);
-        addConnect(6, 5);
-        addConnect(6, 7);
-        addConnect(6, 8);
-        addConnect(6, 9);
-        addConnect(6, 11);
-
-        addConnect(7, 2);
-        addConnect(7, 4);
-        addConnect(7, 6);
-        addConnect(7, 9);
-        addConnect(7, 12);
-
-        addConnect(8, 5);
-        addConnect(8, 6);
-        addConnect(8, 10);
-        addConnect(8, 11);
-
-        addConnect(9, 6);
-        addConnect(9, 7);
-        addConnect(9, 11);
-        addConnect(9, 12);
-
-        addConnect(10, 5);
-        addConnect(10, 8);
-        addConnect(10, 11);
-
-        addConnect(11, 6);
-        addConnect(11, 8);
-        addConnect(11, 9);
-        addConnect(11, 10);
-        addConnect(11, 12);
-
-        addConnect(12, 7);
-        addConnect(12, 9);
-        addConnect(12, 11);
-
-        board[1].setToMid();
-        board[3].setToMid();
-        board[4].setToMid();
-        board[5].setToMid();
-        board[6].setToMid();
-        board[7].setToMid();
-        board[8].setToMid();
-        board[9].setToMid();
-        board[11].setToMid();
-
-        testTheMove = new boolean[13][13][13];
-
-        testTheMove[0][5][10] = true;testTheMove[0][1][2] = true;testTheMove[0][3][6] = true;
-        testTheMove[1][3][5] = true;testTheMove[1][6][11] = true;testTheMove[1][4][7] = true;
-        testTheMove[2][1][0] = true;testTheMove[2][4][6] = true;testTheMove[2][7][12] = true;
-        testTheMove[3][6][9] = true;testTheMove[4][6][8] = true;testTheMove[5][3][1] = true;
-        testTheMove[5][6][7] = true;testTheMove[5][8][11] = true;testTheMove[6][3][0] = true;
-        testTheMove[6][8][10] = true;testTheMove[6][9][12] = true;testTheMove[6][4][2] = true;
-        testTheMove[7][4][1] = true;testTheMove[7][9][11] = true;testTheMove[8][6][4] = true;
-        testTheMove[9][6][3] = true;testTheMove[10][5][0] = true;testTheMove[10][8][6] = true;
-        testTheMove[10][11][12] = true;testTheMove[11][6][1] = true;testTheMove[11][8][5] = true;
-        testTheMove[11][9][7] = true;testTheMove[12][11][10] = true;testTheMove[12][7][2] = true;
+        testTheMove[0][5][10] = true;
+        testTheMove[0][1][2] = true;
+        testTheMove[0][3][6] = true;
+        testTheMove[1][3][5] = true;
+        testTheMove[1][6][11] = true;
+        testTheMove[1][4][7] = true;
+        testTheMove[2][1][0] = true;
+        testTheMove[2][4][6] = true;
+        testTheMove[2][7][12] = true;
+        testTheMove[3][6][9] = true;
+        testTheMove[4][6][8] = true;
+        testTheMove[5][3][1] = true;
+        testTheMove[5][6][7] = true;
+        testTheMove[5][8][11] = true;
+        testTheMove[6][3][0] = true;
+        testTheMove[6][8][10] = true;
+        testTheMove[6][9][12] = true;
+        testTheMove[6][4][2] = true;
+        testTheMove[7][4][1] = true;
+        testTheMove[7][9][11] = true;
+        testTheMove[8][6][4] = true;
+        testTheMove[9][6][3] = true;
+        testTheMove[10][5][0] = true;
+        testTheMove[10][8][6] = true;
+        testTheMove[10][11][12] = true;
+        testTheMove[11][6][1] = true;
+        testTheMove[11][8][5] = true;
+        testTheMove[11][9][7] = true;
+        testTheMove[12][11][10] = true;
+        testTheMove[12][7][2] = true;;
     }
 
     /**
-     * Takes in the array of frog positions, gets the position number, then sets
-     * the board (array of lilyPads) corresponding position to contain a frog
+     * A quick method to print the jumps from the array list for recording jumps
      * 
-     * On the last element in the array, the lilyPad object on the board is set
-     * to say it contains a red frog
+     * @param jumpArrayIn the array list that contains all the jumps, note: it does not
+     * print straight from the jump array list instance variable, because it could be changed
+     * multiple times in the instance
+     */
+    public void printJumps(ArrayList<Integer> jumpArrayIn)
+    {
+        for(int i = 0; i < jumpArrayIn.size()-1; i+=2){
+            System.out.println(jumpArrayIn.get(i)+"-"+jumpArrayIn.get(i+1));
+        }
+    }
+
+    /**
+     * Sets the board to it's initial state, where the frogs locations are determined
+     * by the input array
+     * 
+     * @param frogsIn an array that contains the locations of all the frogs
      */
     public void setBoard(int[] frogsIn)
     {
+        for(int zero = 0; zero < board.length; zero++){//initialize everything to 0, to be safe
+            board[zero] = false;
+        }
+
         remainingFrogs = frogsIn.length;
-        firstFrogPos = frogsIn[0];
-        for(int i = 0; i < frogsIn.length; i++){
+        for(int i = 0; i < frogsIn.length; i++){ 
             int frogPos = frogsIn[i];
-            if(i == board.length-1){
-                board[frogPos].placeFrog();
-                board[frogPos].setToRedFrog();
-            }
-            else{
-                board[frogPos].placeFrog();
+            board[frogPos] = true;
+            if(i == frogsIn.length-1){ //last frog is the red frog, sets the pos the red frog
+                redFrogPos = frogsIn[i];
             }
         }
     }
+    
 
     /**
-     * 
+     * Prints out the final solution, if none found, prints out No Solution Found,
+     * if there is a solution, it prints the jumps and then "Done!"
      */
     public void getSolution()
     {
-        // arraylist that keeps track of jumps
-        ArrayList<Integer> jumps = new ArrayList<Integer>();
-
-        ArrayList<Integer> prevCur = new ArrayList<Integer>();//to keep track of the last jump we made in case we need to backtrack
-        ArrayList<Integer> prevMid = new ArrayList<Integer>();//to keep track of where frogs were
-        int cur = 0;
-        int mid = 0;
-        int land = 0;
-
-        
-        //need to choose a starting point
-        while(board[cur].hasFrog()){
-            cur++;
-            prevCur.set(0, cur);
+        if(solve(board, jumps, remainingFrogs, false) == false){//runs the recursive call
+            System.out.println("No Solution Found!"); //if its false
         }
-        
-        // boolean to stop loop of searching if the solution has not been found
-        boolean solutionFound = false;
-        do{
-            for(mid = 0; mid < 13; mid++){
-                for(land = 0; land < 13; land++){
-                    if(testTheMove[cur][mid][land]){
-                        if((board[mid].hasFrog() && (!board[mid].isRedFrog())) && 
-                        (!board[land].hasFrog())){
-                            testTheMove[cur][mid][land] = false;//move has been used
-                            jumps.add(cur);//saving the jumps
-                            jumps.add(land);//saving the jumps
-                            prevCur.add(cur);//saves the prev cur value at end of list
-                            prevMid.add(mid);//saves prev mid value at end of list
-                            remainingFrogs--;//subtracts a frog from our frog count
-                            board[mid].removeFrog();//removed a frog from our frog count
-                            cur = land;//new cur is our landing position
-                        }
-                        else{continue;}//if there are any issues with the mid having a frog or last
-                        //having a frog, we simply start the loop over to test other land values
-                    }
-                    else if(mid==12 && land == 12){//if we hit all of them, backtrack
-                        prevCur.trimToSize();//ensure the arraylist size is accurate
-                        prevMid.trimToSize();//ensure the arraylist size is accurate
-                        jumps.trimToSize();
-                        cur = prevCur.get(prevCur.size()-1);//get the last cur value and set cur to that
-                        mid = prevMid.get(prevCur.size()-1);//same but for mid
-                        land = jumps.get(jumps.size()-1);//same but for land coming from jumps list
-                        jumps.trimToSize();
-                        jumps.remove(jumps.size()-1);
-                        
-                        testTheMove[cur][mid][land] = true;
-                        remainingFrogs++;
-                        board[mid].placeFrog();
-                    }
-                    else if(board[land].isRedFrog()&&remainingFrogs==2){
-                        board[cur].setToRedFrog();
-                        board[land].removeRedFrog();
-                        board[mid].removeFrog();
-                        remainingFrogs--;
-                        mid = 14;
-                        break;
-                    }
-                    else{continue;}//if we have not hit all them, but the move is not valid, restart
-                    //the loop and keep trying
-                }
-            }
-        }while(remainingFrogs > 1);
-        System.out.println(jumps.toString()+"\n");
-        System.out.println("Done!");
+        else{
+            printJumps(jumps); //if the recursive call ends up being true
+            System.out.println("Done!");
+        }
     }
 
     /**
+     * The method to solve the actual problem
      * 
+     * @param boardIn a boolean array that becomes a duplicate of the board array
+     * @param jumpsIn an array list that contains integer representing the jumps
+     * @param remainingFrogsIn the amount of frogs remaining on the board
+     * @param solutionFound if a solution is found for the jump, return true, false otherwise
+     * 
+     * @return boolean represents the solution found or not, if it is finally true,
+     *  it means the method found it all to be true
      */
-    public void solve()
+    public boolean solve(boolean[] boardIn, ArrayList<Integer> jumpsIn, 
+    int remainingFrogsIn, boolean solutionFound)
     {
+        boolean[] newBoard = boardIn; // duplicate the board to avoid overwriting
+
+        if(remainingFrogsIn < 2){//base case if there are already less than or there are 
+            //two frogs on the board
+            for(int i = 0; i < newBoard.length; i++){
+                if((newBoard[i] == true)&&(i==redFrogPos))
+                {
+                    jumps = jumpsIn;
+                    solutionFound = true;
+                    return solutionFound;
+                }
+            }
+            return solutionFound;
+        }
+        else 
+        {
+            for(int cur = 0; cur < 13; cur++){
+                for(int mid = 0; mid < 13; mid++){
+                    for(int land = 0; land < 13; land++){
+                        if(testTheMove[cur][mid][land] == true){
+                            if((newBoard[cur] == true)&&(newBoard[mid] == true)
+                            &&(newBoard[land] == false)){
+                                if(mid == redFrogPos){
+                                    continue;
+                                }
+                                if(cur==redFrogPos){
+                                    redFrogPos = land;
+                                }
+                                newBoard[cur]=false;
+                                newBoard[mid]=false;
+                                newBoard[land]=true;
+                                jumpsIn.add(cur);
+                                jumpsIn.add(land);
+                                solutionFound = solve(newBoard, jumpsIn, remainingFrogsIn-1, solutionFound);
+                                if(solutionFound == false){
+                                    newBoard[cur]=true;
+                                    newBoard[mid]=true;
+                                    newBoard[land]=false;
+                                    jumpsIn.remove(jumpsIn.size()-1);
+                                    jumpsIn.remove(jumpsIn.size()-1);
+                                    if(land==redFrogPos){
+                                        redFrogPos = cur;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return solutionFound;
     }
 }
